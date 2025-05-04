c34f2ce40c1ece6a61c97d9021b8aa62e0da7f3b
alerezaaa <86428901+alerezaaa@users.noreply.github.com>
2025-05-04 07:45:36 +0000

    Deploying to testing from @ alerezaaa/Jekyll_alireza@6acc976c2a8354743cdbbb57c8e2c773430eb931 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -168,6 +168,13 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
+        },
+      },{
+        id: 'social-custom_social2',
+        title: 'Custom_social2',
+        section: 'Socials',
+        handler: () => {
           window.open("https://poa.st/@alireza", "_blank");
         },
       },{
