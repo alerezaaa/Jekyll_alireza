ee2e2243f148f77e7680a68fdd451a581c7f5e6f
alerezaaa <86428901+alerezaaa@users.noreply.github.com>
2026-02-21 08:19:53 +0000

    Deploying to cf-pages from @ alerezaaa/Jekyll_alireza@1eb271b7d77a301940acac5ca5e32f52478e4077 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -101,15 +101,15 @@ ninja.data = [{
             window.location.href = "/blog/2022/i-ve-just-finished-machine-learning-course/";
           
         },
-      },{id: "post-https-www-youtube-com-watch-v-egn-c9lx9di",
+      },{id: "post-my-youtube-video-about-our-rasberry-pi-project",
         
-          title: 'https://www.youtube.com/watch?v=egN-C9lx9DI <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: 'My Youtube video about our Rasberry Pi project <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
-        description: "",
+        description: "We were working on a project which the goal was creating a part of a hardware to detect and send &quot;cut signal&quot; to proper section of the device based on Image processing",
         section: "Posts",
         handler: () => {
           
-            window.open("https://www.youtube.com/watch?v=egN-C9lx9DI", "_blank");
+            window.open("https://youtu.be/egN-C9lx9DI", "_blank");
           
         },
       },{id: "books-animal-farm",
