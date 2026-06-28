9ecc658b83f6fe640cad893ef1168c948bcaec71
alerezaaa <86428901+alerezaaa@users.noreply.github.com>
2026-06-28 11:32:23 +0000

    Deploying to cf-pages from @ alerezaaa/Jekyll_alireza@88e2b39565fb580868f0acf22b89664cc8622e61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -128,6 +128,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/multi-cloud_infrastructure/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
             },},{id: "projects-basil-harvester",
           title: 'Basil Harvester',
           description: "Design a machine to harvest basils as a successor to Scissor Jack",
